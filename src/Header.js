9348160698503581745aac206d0305ab09977cb8
HaarisMalik <haaris.malik416@gmail.com>
2022-07-05 20:43:35 -0400

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './Header.css'
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import AppsIcon from '@mui/icons-material/Apps'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import { Avatar, IconButton } from '@mui/material';
+
+
+function Header() {
+    return (
+        <div className="header">
+           <div className="header__left">
+            <IconButton>
+             <MenuIcon />
+             </IconButton>
+             <img src="https://www.amocrm.com/static/images/pages/integrations/logo/gmail.png" alt=""/>
+           </div>
+
+           <div className="header__middle">
+             <SearchIcon />
+            <input placeholder="Search Mail" type="text"/>
+            <ArrowDropDownIcon className="header__inputCaret" />
+           </div>
+
+
+           <div className="header__right">
+           <IconButton>
+             <AppsIcon/>
+             </IconButton>
+             <IconButton>
+             <NotificationsIcon/>
+             </IconButton>
+             <Avatar />
+           </div>
+        </div>
+    )
+}
+
+export default Header
